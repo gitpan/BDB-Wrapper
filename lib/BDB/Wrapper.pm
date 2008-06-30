3ab4d0b07029ccd56e68fe186973eaeff536bf8e
栗田　創 <hikarine@cpan.org>
2008-06-30 11:32:57 +0000

    Import of HIKARINE/BDB-Wrapper-0.13 from CPAN.
    gitpan-cpan-distribution: BDB-Wrapper
    gitpan-cpan-version:      0.13
    gitpan-cpan-path:         HIKARINE/BDB-Wrapper-0.13.tar.gz
    gitpan-cpan-author:       HIKARINE
    gitpan-cpan-maturity:     released

--- lib/BDB/Wrapper.pm
+++ lib/BDB/Wrapper.pm
@@ -12,29 +12,80 @@ our @ISA = qw(Exporter AutoLoader);
 our %EXPORT_TAGS = ( 'all' => [ qw() ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw();
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 
 =head1 NAME
-
 BDB::Wrapper Wrapper module for BerkeleyDB.pm
-
 This will make it easy to use BerkeleyDB.pm.
-
 You can protect bdb file from the concurrent access and you can use BerkeleyDB.pm with less difficulty.
+This module is used on http://www.accessup.org/ and is developed based on the requirement.
 
 Attention: If you use this module for the specified Berkeley DB file,
 please use this module for all access to the bdb.
 By it, you can control lock to the bdb file.
 Lock files are created under /tmp/bdb_home.
 If you set ram 1 in new option, lock files are created under /dev/shm/bdb_home.
-
 =cut
 
+=head1 Example
+package test_bdb;
+use BDB::Wrapper;
+
+my $pro=new test_bdb;
+$pro->run();
+
+sub new(){
+  my $self={};
+  return bless $self;
+}
+
+sub run(){
+  my $self=shift;
+  $self->init_vars();
+  $self->demo();
+}
+
+sub init_vars(){
+  my $self=shift;
+  $self->{'bdb'}='/tmp/test.bdb';
+  $self->{'bdbw'}=new BDB::Wrapper;
+}
+
+sub demo(){
+  my $self=shift;
+  # Open db handler for writing
+  if(my $dbh=$self->{'bdbw'}->create_write_dbh($self->{'bdb'})){
+    # Ignore Ctr+C while putting to avoid to destruct bdb.
+    local $SIG{'INT'}='IGNORE';
+    local $SIG{'TERM'}='IGNORE';
+    local $SIG{'QUIT'}='IGNORE';
+    # put
+    if($dbh->db_put('name', 'value')==0){
+    }
+    else{
+      die 'Failed to put to '.$self->{'bdb'};
+    }
+    $dbh->db_close();
+  }
+
+  # Open db handler for reading
+  if(my $dbh=$self->{'bdbw'}->create_read_dbh($self->{'bdb'})){
+    my $value;
+    # read
+    if($dbh->db_get('name', $value)==0){
+      print 'Name='.$name.' value='.$value."\n";
+    }
+    $dbh->db_close();
+  }
+}
+=cut
+
+=head1 methods
 
 =head2 new
-
 Creates an object of BDB::Wrapper
-
+If you set {'ram'=>1}, you can use /dev/shm for storing locking file for BDB.
+If you set {'wait'=>wait_seconds}, you can specify the seconds in which dead lock will be removed.
 =cut
 
 sub new(){
@@ -45,7 +96,7 @@ sub new(){
   if($op_ref->{'ram'}){
     $self->{'lock_root'}='/dev/shm';
   }
-  $self->{'wait'}=11; #ŠJ‚­‚Ì‚ÉŽžŠÔ‚©‚©‚è‰ß‚¬
+  $self->{'wait'}=$op_ref->{'wait'} || 11; #ŠJ‚­‚Ì‚ÉŽžŠÔ‚©‚©‚è‰ß‚¬
   return bless $self;
 }
 
@@ -53,9 +104,7 @@ sub new(){
 __END__
 
 =head2 create_env
-
 Creates Environment for BerkeleyDB
-
 =cut
 
 sub create_env(){
@@ -72,7 +121,7 @@ sub create_env(){
     }
     $env = new BerkeleyDB::Env {
       -Flags => DB_INIT_CDB | DB_CREATE | DB_INIT_MPOOL,
-      -Home  => $home_dir
+      -Home => $home_dir
       };
   }
   else{
@@ -87,12 +136,23 @@ sub create_env(){
 }
 # { DB_DATA_DIR => "/home/databases",                          DB_LOG_DIR  => "/home/logs",                          DB_TMP_DIR  => "/home/tmp"
 
+
+=head2 create_dbh
+Creates database handler for BerkeleyDB
+This will be obsolete due to too much simplicity, so please don\'t use.
+=cut
+
 sub create_dbh(){
   my $self=shift;
   my $bdb=File::Spec->rel2abs(shift);
   my $op=shift;
   return $self->create_write_dbh($bdb,$op);
 }
+
+=head2 create_hash_ref
+Creates database handler for BerkeleyDB
+This will be obsolete due to too much simplicity, so please don\'t use.
+=cut
 
 sub create_hash_ref(){
   my $self=shift;
@@ -101,26 +161,16 @@ sub create_hash_ref(){
   return $self->create_write_hash_ref($bdb, $op);
 }
 
-=head2
-
+=head2 create_write_dbh
 This will creates database handler for writing.
-
 $self->create_write_dbh($bdb, {'hash'=>0 or 1, 'dont_try'=>0 or 1, 'sort_code_ref'=>$sort_code_reference, 'sort' or 'sort_num'=>0 or 1, 'reverse_cmp'=>0 or 1, 'reverse' or 'reverse_num'=>0 or 1});
-
 In the default mode, BDB file will be created as Btree;
-
 If you set 'hash' 1, Hash BDB will be created.
-
 If you set 'dont_try' 1, this module won\'t try to unlock BDB if it detects the situation in which deadlock may be occuring.
-
 If you set sort_code_ref some code reference, you can set subroutine for sorting for Btree.
-
 If you set sort or sort_num 1, you can use sub {$_[0] <=> $_[1]} for sort_code_ref.
-
 If you set reverse or reverse_num 1, you can use sub {$_[1] <=> $_[0]} for sort_code_ref.
-
 If you set reverse_cmp 1, you can use sub {$_[1] cmp $_[0]} for sort_code_ref.
-
 =cut
 
 sub create_write_dbh(){
@@ -206,24 +256,15 @@ sub create_write_dbh(){
 }
 
 
-=head2
-
+=head2 create_read_dbh
 This will creates database handler for reading.
-
 $self->create_read_dbh($bdb, {'hash'=>0 or 1, 'dont_try'=>0 or 1, 'sort_code_ref'=>$sort_code_reference, 'sort' or 'sort_num'=>0 or 1, 'reverse_cmp'=>0 or 1, 'reverse' or 'reverse_num'=>0 or 1});
-
 In the default mode, BDB file will be created as Btree;
-
 If you set 'hash' 1, Hash BDB will be created.
-
 If you set 'dont_try' 1, this module won\'t try to unlock BDB if it detects the situation in which deadlock may be occuring.
-
 If you set sort_code_ref some code reference, you can set subroutine for sorting for Btree.
-
 If you set sort or sort_num 1, you can use sub {$_[0] <=> $_[1]} for sort_code_ref.
-
 If you set reverse or reverse_num 1, you can use sub {$_[1] <=> $_[0]} for sort_code_ref.
-
 If you set reverse_cmp 1, you can use sub {$_[1] cmp $_[0]} for sort_code_ref.
 =cut
 
@@ -307,24 +348,15 @@ sub create_read_dbh(){
 }
 
 
-=head2
-
+=head2 create_write_hash_ref
 This will creates hash for writing.
-
 $self->create_write_hash_ref($bdb, {'hash'=>0 or 1, 'dont_try'=>0 or 1, 'sort_code_ref'=>$sort_code_reference,  'sort' or 'sort_num'=>0 or 1, 'reverse_cmp'=>0 or 1, 'reverse' or 'reverse_num'=>0 or 1});
-
 In the default mode, BDB file will be created as Btree;
-
 If you set 'hash' 1, Hash BDB will be created.
-
 If you set 'dont_try' 1, this module won\'t try to unlock BDB if it detects the situation in which deadlock may be occuring.
-
 If you set sort_code_ref some code reference, you can set subroutine for sorting for Btree.
-
 If you set sort or sort_num 1, you can use sub {$_[0] <=> $_[1]} for sort_code_ref.
-
 If you set reverse or reverse_num 1, you can use sub {$_[1] <=> $_[0]} for sort_code_ref.
-
 If you set reverse_cmp 1, you can use sub {$_[1] cmp $_[0]} for sort_code_ref.
 =cut
 
@@ -412,26 +444,16 @@ sub create_write_hash_ref(){
   return \%hash;
 }
 
-=head2
-
+=head2 create_read_hash_ref
 This will creates database handler for reading.
-
 $self->create_read_hash_ref($bdb, 'hash'=>0 or 1, 'dont_try'=>0 or 1, 'sort_code_ref'=>$sort_code_reference, 'sort' or 'sort_num'=>0 or 1, 'reverse_cmp'=>0 or 1, 'reverse' or 'reverse_num'=>0 or 1});
-
 In the default mode, BDB file will be created as Btree;
-
 If you set 'hash' 1, Hash BDB will be created.
-
 If you set 'dont_try' 1, this module won\'t try to unlock BDB if it detects the situation in which deadlock may be occuring.
-
 If you set sort_code_ref some code reference, you can set subroutine for sorting for Btree.
-
 If you set sort or sort_num 1, you can use sub {$_[0] <=> $_[1]} for sort_code_ref.
-
 If you set reverse or reverse_num 1, you can use sub {$_[1] <=> $_[0]} for sort_code_ref.
-
 If you set reverse_cmp 1, you can use sub {$_[1] cmp $_[0]} for sort_code_ref.
-
 =cut
 
 sub create_read_hash_ref(){
